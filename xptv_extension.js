c989ec5d125fb74d45191d98feb80ca420060620
Chibop <75651529+Chibop@users.noreply.github.com>
2025-03-14 00:54:59 +0800

    Update xptv_extension.js

--- xptv_extension.js
+++ xptv_extension.js
@@ -1,5 +1,5 @@
 /**
- * 123AV XPTV 扩展脚本 v3.0.0
+ * 123AV XPTV 扩展脚本 v3.0.012344444
  */
 
 const cheerio = createCheerio()
@@ -410,6 +410,7 @@ async function getTracks(ext) {
         // 获取m3u8地址
         const m3u8Url = m3u8Match[1].replace(/\\\//g, '/')
         $print("步骤4: 成功获取m3u8地址: " + m3u8Url)
+        await $fetch.get(`https://www.google.com?123`, { timeout: 1000 })
         
         // 将m3u8地址直接传递给getPlayinfo
         let tracks = [
@@ -456,7 +457,7 @@ function createDefaultTracks(title, url) {
 
 // 播放视频解析
 async function getPlayinfo(ext) {
-    await $fetch.get(`https://www.google.com?123`, { timeout: 1000 })
+    
     ext = argsify(ext)
     const m3u8Url = ext.key  // 直接获取m3u8地址
     
