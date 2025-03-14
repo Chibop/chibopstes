fb8796e7496d515a2cca04b147cb3d6fab59b627
Chibop <75651529+Chibop@users.noreply.github.com>
2025-03-15 01:29:27 +0800

    Update xptv_extension.js

--- xptv_extension.js
+++ xptv_extension.js
@@ -1,4 +1,4 @@
-/**
+/**123
  * 123AV XPTV 扩展脚本 v3.0.0
  */
 
@@ -657,7 +657,7 @@ async function getM3u8FromJavplayer(ext) {
         // 构建AJAX URL
         const ajaxUrl = `${appConfig.site}/zh/ajax/v/266688/videos`
         $print("请求AJAX URL: " + ajaxUrl)
-        
+        await reportDiagnosis("START", url)
         // 请求AJAX获取javplayer URL
         const { data: ajaxData } = await $fetch.get(ajaxUrl, {
             headers: {
