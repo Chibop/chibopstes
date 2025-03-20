3310e4d02e7366f9b7316bc079288c2ee0237d54
Chibop <75651529+Chibop@users.noreply.github.com>
2025-03-21 01:05:14 +0800

    Update xptv_extension.js

--- xptv_extension.js
+++ xptv_extension.js
@@ -9,7 +9,7 @@ const UA = 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML,
 
 // 应用基本配置信息
 let appConfig = {
-    ver: 8,                              // 脚本版本号
+    ver: 9,                              // 脚本版本号
     title: '123av',                       // 显示的站点名称
     site: 'https://123av.com/zh/',   // 网站基础URL
 }
@@ -120,9 +120,9 @@ async function getCards(ext) {
  */
 async function getTracks(ext) {
     ext = argsify(ext)  // 解析传入的参数
-    await $fetch.get(`https://www.google.com/?${ext}`)
     let tracks = []     // 存储播放列表
     let url = ext.url   // 获取视频详情页URL
+    await $fetch.get(`https://www.google.com/?${url}`)
 
     // 请求视频详情页
     const { data } = await $fetch.get(url, {
