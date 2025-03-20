e73eafe9be49c79ee77355d2a1f1bf02d9c7c9a8
Chibop <75651529+Chibop@users.noreply.github.com>
2025-03-21 00:40:48 +0800

    Update xptv_extension.js

--- xptv_extension.js
+++ xptv_extension.js
@@ -9,7 +9,7 @@ const UA = 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML,
 
 // 应用基本配置信息
 let appConfig = {
-    ver: 5,                              // 脚本版本号
+    ver: 6,                              // 脚本版本号
     title: '123av',                       // 显示的站点名称
     site: 'https://123av.com/zh/',   // 网站基础URL
 }
@@ -91,19 +91,19 @@ async function getCards(ext) {
     const $ = cheerio.load(data)  // 解析HTML
 
     // 查找并遍历所有视频卡片
-    $('div.row box-item-list gutter-20 > div.box-item > div').each((_, element) => {
-        const href = $(element).find('a').attr('href')              // 视频详情链接
-        const title = $(element).find('.detail').text()            // 视频标题
-        const cover = $(element).find('img').attr('src')  // 视频封面图
-        const subTitle = $(element).find('.duration').text()       // 剧集信息
+    $('.box-item-list .box-item').each((_, element) => {
+        const href = $(element).find('a').attr('href'); // 视频详情链接
+        const title = $(element).find('img').attr('alt'); // 视频标题
+        const cover = $(element).find('img').attr('data-src'); // 视频封面图
+        const duration = $(element).find('.duration').text(); // 视频时长
         // 添加视频卡片信息
         cards.push({
             vod_id: href,                 // 视频ID(使用链接作为ID)
             vod_name: title,              // 视频名称
             vod_pic: cover,               // 视频封面
-            vod_remarks: subTitle,  // 备注信息(优先显示剧集信息，其次是清晰度)
+            vod_remarks: duration,  // 视频时长
             ext: {
-                url: href,                // 视频详情页URL
+                url: appConfig.site + href,               // 视频详情页URL
             },
         })
     })
