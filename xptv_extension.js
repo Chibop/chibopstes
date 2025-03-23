4a90075a8f8a38a945b169e0da16c0cd5a9a007b
Chibop <75651529+Chibop@users.noreply.github.com>
2025-03-23 10:37:03 +0800

    Update xptv_extension.js

--- xptv_extension.js
+++ xptv_extension.js
@@ -9,7 +9,7 @@ const UA = 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML,
 
 // 应用基本配置信息
 let appConfig = {
-    ver: 63,                              // 脚本版本号
+    ver: 64,                              // 脚本版本号
     title: '123av',                       // 显示的站点名称
     site: 'https://123av.com/zh/',   // 网站基础URL
 }
@@ -170,7 +170,7 @@ async function getTracks(ext) {
         list: [
             {
                 title: '默认分组',      // 分组标题
-                tracks,                 // 播放列表
+                flattenedArray,                 // 播放列表
             },
         ],
     })
@@ -268,6 +268,7 @@ async function processUrls(urls) {
                 const url = item.url;   // 获取 url
                 cards.push({
                     name: name,              // 视频名称
+                    pan: '',
                     ext: {
                         url: url,               // 视频详情页URL
                     },
