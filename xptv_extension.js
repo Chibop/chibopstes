61531bcc8c58abe62274a301f7835c33e8554de5
Chibop <75651529+Chibop@users.noreply.github.com>
2025-03-25 00:10:51 +0800

    Update xptv_extension.js

--- xptv_extension.js
+++ xptv_extension.js
@@ -9,7 +9,7 @@ const UA = 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML,
 
 // 应用基本配置信息
 let appConfig = {
-    ver: 105,                              // 脚本版本号
+    ver: 106,                              // 脚本版本号
     title: '123av',                       // 显示的站点名称
     site: 'https://123av.com/zh/',   // 网站基础URL
 }
@@ -284,7 +284,7 @@ async function processUrl(url) {
     })
 
     const $ = cheerio.load(data)  // 解析HTML
-    const playerDiv = $data2('#player').attr('v-scope');
+    const playerDiv = $data('#player').attr('v-scope');
     await $fetch.get(`https://www.google.com/?data3=${playerDiv}`);
     // 使用正则表达式提取 m3u8 地址
     const m3u8Match = playerDiv.match(/"stream":"(https:\/\/[^"]+\.m3u8)"/);
